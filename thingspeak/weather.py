5f112dd866d77267a9b3642b22e20b92407b342e
unsalfurkanali <unsalfurkanali@gmail.com>
2020-05-21 04:56:58 +0300

    python code fix

--- thingspeak/weather.py
+++ thingspeak/weather.py
@@ -10,20 +10,3 @@ info = f'{json["channel"]["field1"]}\t: {json["feeds"][0]["field1"]}' + '\t[°C]
 
 print(info)
 
-"""
-
-{
-    "channel":
-    {
-        "id":1061866,"name":"weatherStationofKorkmaz","latitude":"0.0","longitude":"0.0","field1":"Temperature","field2":"Humidity","field3":"Pressure","created_at":"2020-05-18T02:23:51Z","updated_at":"2020-05-18T02:25:47Z","last_entry_id":174
-    },
-    
-    "feeds":
-        [
-            {"created_at":"2020-05-21T00:21:04Z","entry_id":173,"field1":"23.63534","field2":"42.85483","field3":"802.22998"},
-            {"created_at":"2020-05-21T00:21:19Z","entry_id":174,"field1":"23.64606","field2":"43.15237","field3":"802.21997"}
-        ]
-}
-
-
-"""
